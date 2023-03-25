f151a2c589b0192331e1d782c2960e276698b5d1
Gabrielmarquezin <Gabrielmarquesaraujs@gmail.com>
2023-03-24 23:15:58 -0300

    create farm routes

---
+++ backend/src/routes/framework/middleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Boringue\Backend\routes\framework;
+
+class Middleware{
+    private $action;
+    private $middlewareBefore = [];
+    private $middlewareAfter = [];
+
+    public function __construct($action)
+    {
+        $this->$action = $action;
+    }
+
+    public function before($middleware)
+    {
+        $this->middlewareBefore[] = $middleware;
+        return $this;
+    }
+
+    public function after($middleware)
+    {
+        $this->middlewareAfter[] = $middleware;
+        return $this;
+    }
+
+    public function getData(){
+        return[
+            'action' => $this->action,
+            'before' => $this->middlewareBefore,
+            'after'  => $this->middlewareAfter
+        ];
+    }
+}
---
+++ backend/src/routes/framework/route.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Boringue\Backend\routes\framework;
+use Boringue\Backend\routes\framework;
+
+class Router{
+    public $routes = [];
+    private $method;
+    private $path;
+    private $params;
+
+    public function __construct(string $method, $path)
+    {
+        $this->method = $method;
+        $this->path = $path;
+    }
+
+    public function get(string $route, $action)
+    {
+        return $this->add('GET', $route, $action);
+    }
+
+    public function post(string $route, $action)
+    {
+        return $this->add('POST', $route, $action);
+    }
+
+    private function add(string $method, $route, $action)
+    {
+        return $this->routes[$method][$route] = new Middleware($action);
+    }
+
+    public function handler()
+    {
+        if (empty($this->routes[$this->method])) {
+            return false;
+        }
+
+        if (isset($this->routes[$this->method][$this->path])) {
+            return $this->routes[$this->method][$this->path];
+        }
+
+        foreach ($this->routes[$this->method] as $route => $action) {
+            $result = $this->checkUrl($route, $this->path);
+            if ($result >= 1) {
+                return $action;
+            }
+        }
+
+        return false;
+    }
+
+    private function checkUrl(string $route, $path)
+    {
+        preg_match_all('/\{([^\}]*)\}/', $route, $variables);
+
+        $regex = str_replace('/', '\/', $route);
+
+        foreach ($variables[0] as $k => $variable) {
+            $replacement = '([a-zA-Z0-9\-\_\ ]+)';
+            $regex = str_replace($variable, $replacement, $regex);
+        }
+
+        $regex = preg_replace('/{([a-zA-Z]+)}/', '([a-zA-Z0-9+])', $regex);
+        $result = preg_match('/^' . $regex . '$/', $path, $params);
+        $this->params = $params;
+
+        return $result;
+    }
+}
