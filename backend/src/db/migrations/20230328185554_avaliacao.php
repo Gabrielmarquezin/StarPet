5592b6bc3add58ebf336dd17a6c71636e3b834d3
Gabriel Marques <106358406+Gabrielmarquezin@users.noreply.github.com>
2023-03-28 16:41:14 -0300

    migration uploads

---
+++ backend/src/db/migrations/20230328185554_avaliacao.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Avaliacao extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $produto_avalicao = $this->table('avaliacao', ['id' => 'cod']);
+        $produto_avalicao->addColumn('quantidade_stars', 'integer', ['null' => false ])
+                         ->addColumn('mensagem', 'string', ['null' => false,'limit' => '150'])
+
+                         ->addColumn('cod_user', 'integer', ["signed" => false])
+                         ->addColumn('cod_produto', 'integer', ["signed" => false])
+
+                         ->addForeignKey('cod_user', 'users', 'cod', ['delete' => 'RESTRICT', 'update' => 'NO_ACTION'])
+                         ->addForeignKey('cod_produto', 'produto', 'cod', ['delete' => 'RESTRICT', 'update' => 'NO_ACTION'])
+
+                         ->create();
+    }
+}
