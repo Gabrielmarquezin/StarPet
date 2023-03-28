1ec6a2b12871ab38181f3d58829fb00d7d28cba9
Gabriel Marques <106358406+Gabrielmarquezin@users.noreply.github.com>
2023-03-28 16:55:56 -0300

    making migrations

---
+++ backend/src/db/migrations/20230328194846_ficha_pet.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class FichaPet extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $ficha_pet = $this->table('ficha_pet', ['id' => 'cod']);
+        $ficha_pet ->addColumn('raca', 'string', ['limit' => '30'])
+                   ->addColumn('alergias', 'string', ['limit' => '45'])
+                   ->addColumn('observacoes', 'string', ['limit' => '150'])
+                   ->addColumn('tamanho', 'string', ['limit' => '45'])
+                   ->addColumn('estoque', 'string', ['limit' => '20'])
+
+                   ->create();
+    }
+}
